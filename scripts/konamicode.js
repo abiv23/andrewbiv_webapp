5facdb0c8fb1f5916bfd598d2bc5f9628ddf913a
“Andrew <abiv23@gmail.com>
2017-07-11 17:03:51 -0600

    konamicode working

---
+++ scripts/konamicode.js
@@ -0,0 +1,36 @@
+// Cheat Codes
+neededkeys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65], started = false, count = 0;
+$(document).keydown(function(e) {
+    key = e.keyCode;
+    if (!started) {
+        console.log('keydown')
+        if (key == 38) {
+            started = true;
+        }
+    }
+    if (started) {
+        console.log('code listener running')
+        if (neededkeys[count] == key) {
+            count++;
+        } else {
+            reset();
+        }
+        if (count == 10) {
+            reset();
+            // Do your stuff here
+            alert('Cheat Codes Activated');
+            $('body').css('background-color', '#FFA8A8');
+            // Turn down for what
+            var s = document.createElement('script');
+            s.setAttribute('src', 'https://nthitz.github.io/turndownforwhatjs/tdfw.js');
+            document.body.appendChild(s);
+        }
+    } else {
+        reset();
+    }
+});
+
+function reset() {
+    started = false;
+    count = 0;
+}
