1bddfad543c7b4af4dee6a695f780ed866593458
“Andrew <abiv23@gmail.com>
2017-08-02 21:05:13 -0600

    scrollfire contact

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,51 @@
 $(document).ready(function() {
     materializeParallax();
+    Materialize.scrollFire(options);
 })
 
 /* vanashing top nav */
 
-$("#nav-drop").hide();
+// $("#nav-drop").hide();
+//
+// $("#cityclick").mouseover(function() {
+//     $("#citydrop").slideDown('slow');
+// });
+//
+// $("#wrapper").mouseleave(function() {
+//     $("#citydrop").slideUp('slow');
+// });
 
-$("#cityclick").mouseover(function() {
-    $("#citydrop").slideDown('slow');
-});
+/* scrollfire initialize*/
+var options = [{
+    selector: '#scrollToast',
+    offset: 20,
+    callback: function(el) {
+        Materialize.toast("Conatct me at abiv23@gmail.com", 8000)
+    }
 
-$("#wrapper").mouseleave(function() {
-    $("#citydrop").slideUp('slow');
-});
+}, {
+    selector: '#scrollToast',
+    offset: 200,
+    callback: function() {}
+}];
+
+
+/* scrollfire examples */
+// var options = [
+//       {selector: '#staggered-test', offset: 50, callback: function(el) {
+//         Materialize.toast("This is our ScrollFire Demo!", 1500 );
+//       } },
+//       {selector: '#staggered-test', offset: 205, callback: function(el) {
+//         Materialize.toast("Please continue scrolling!", 1500 );
+//       } },
+//       {selector: '#staggered-test', offset: 400, callback: function(el) {
+//         Materialize.showStaggeredList($(el));
+//       } },
+//       {selector: '#image-test', offset: 500, callback: function(el) {
+//         Materialize.fadeInImage($(el));
+//       } }
+//     ];
+//     Materialize.scrollFire(options);
 
 /* materialize toast */
 /* for introduction of KonamiCode, still needs to be intergrated */
@@ -21,32 +54,32 @@ $("#wrapper").mouseleave(function() {
 
 /* materialize scrollfire */
 /* for intro of project cards with animation not intergrated yet */
-var options = [{
-    selector: '#staggered-test',
-    offset: 50,
-    callback: function(el) {
-        Materialize.toast("This is our ScrollFire Demo!", 1500);
-    }
-}, {
-    selector: '#staggered-test',
-    offset: 205,
-    callback: function(el) {
-        Materialize.toast("Please continue scrolling!", 1500);
-    }
-}, {
-    selector: '#staggered-test',
-    offset: 400,
-    callback: function(el) {
-        Materialize.showStaggeredList($(el));
-    }
-}, {
-    selector: '#image-test',
-    offset: 500,
-    callback: function(el) {
-        Materialize.fadeInImage($(el));
-    }
-}];
-Materialize.scrollFire(options);
+// var options = [{
+//     selector: '#staggered-test',
+//     offset: 50,
+//     callback: function(el) {
+//         Materialize.toast("This is our ScrollFire Demo!", 1500);
+//     }
+// }, {
+//     selector: '#staggered-test',
+//     offset: 205,
+//     callback: function(el) {
+//         Materialize.toast("Please continue scrolling!", 1500);
+//     }
+// }, {
+//     selector: '#staggered-test',
+//     offset: 400,
+//     callback: function(el) {
+//         Materialize.showStaggeredList($(el));
+//     }
+// }, {
+//     selector: '#image-test',
+//     offset: 500,
+//     callback: function(el) {
+//         Materialize.fadeInImage($(el));
+//     }
+// }];
+// Materialize.scrollFire(options);
 
 /* materializeParallax */
 
