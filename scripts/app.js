f72462b77027c33147c370e6a3469ef47acab245
“Andrew <abiv23@gmail.com>
2017-08-12 11:33:37 -0600

    clock js fix

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,23 @@
 $(document).ready(function() {
-    materializeParallax();
     Materialize.scrollFire(options);
+    setInterval(setDateFooter, 1000);
 })
 
-/* vanashing top nav */
-
-// $("#nav-drop").hide();
-//
-// $("#cityclick").mouseover(function() {
-//     $("#citydrop").slideDown('slow');
-// });
-//
-// $("#wrapper").mouseleave(function() {
-//     $("#citydrop").slideUp('slow');
-// });
+function setDateFooter() {
+    const hoursHand = $('.footer-hours-hand');
+    const secondHand = $('.footer-second-hand');
+    const minsHand = $('.footer-min-hand');
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const mins = now.getMinutes();
+    const hours = now.getHours();
+    const secondsToDegrees = (((seconds / 60) * 360) + 90); /* plus 90deg is to account for initial offset */
+    const minsToDegrees = (((mins / 60) * 360) + ((seconds / 60) * 6) + 90); /* plus 90deg is to account for initial offset */
+    const hoursToDegrees = (((hours / 12) * 360) + ((mins / 60) * 30) + 90); /* plus 90deg is to account for initial offset */
+    secondHand.css('transform', `rotate(${secondsToDegrees}deg)`);
+    minsHand.css('transform', `rotate(${minsToDegrees}deg)`);
+    hoursHand.css('transform', `rotate(${hoursToDegrees}deg)`);
+}
 
 /* scrollfire initialize*/
 var options = [{
@@ -97,6 +101,19 @@ var options = [{
 
 /* materializeParallax */
 
-function materializeParallax() {
-    $('.parallax').parallax();
-}
+// function materializeParallax() {
+//     $('.parallax').parallax();
+// }
+
+
+/* vanashing top nav */
+
+// $("#nav-drop").hide();
+//
+// $("#cityclick").mouseover(function() {
+//     $("#citydrop").slideDown('slow');
+// });
+//
+// $("#wrapper").mouseleave(function() {
+//     $("#citydrop").slideUp('slow');
+// });
--- scripts/clock.js
+++ scripts/clock.js
@@ -1,39 +1,18 @@
 $(document).ready(function() {
     setInterval(setDate, 1000);
-    setInterval(setDateFooter, 1000);
 })
 
 function setDate() {
     const secondHand = $('.second-hand');
     const minsHand = $('.min-hand');
-    const hoursHand = $('hour-hand');
+    const hoursHand = $('.hour-hand');
     const now = new Date();
     const seconds = now.getSeconds();
     const mins = now.getMinutes();
     const hours = now.getHours();
     const secondsToDegrees = (((seconds / 60) * 360) + 90); /* plus 90deg is to account for initial offset */
-    const minsToDegrees = (((mins / 60) * 360) + 90); /* plus 90deg is to account for initial offset */
-    const hoursToDegrees = (((hours / 12) * 360) + 90); /* plus 90deg is to account for initial offset */
-    console.log(hours);
-    console.log(hoursToDegrees);
-    secondHand.css('transform', `rotate(${secondsToDegrees}deg)`);
-    minsHand.css('transform', `rotate(${minsToDegrees}deg)`);
-    hoursHand.css('transform', `rotate(${hoursToDegrees}deg)`);
-}
-
-function setDateFooter() {
-    const secondHand = $('.footer-second-hand');
-    const minsHand = $('.footer-min-hand');
-    const hoursHand = $('footer-hour-hand');
-    const now = new Date();
-    const seconds = now.getSeconds();
-    const mins = now.getMinutes();
-    const hours = now.getHours();
-    const secondsToDegrees = (((seconds / 60) * 360) + 90); /* plus 90deg is to account for initial offset */
-    const minsToDegrees = (((mins / 60) * 360) + 90); /* plus 90deg is to account for initial offset */
-    const hoursToDegrees = (((hours / 12) * 360) + 90); /* plus 90deg is to account for initial offset */
-    console.log(hours);
-    console.log(hoursToDegrees);
+    const minsToDegrees = (((mins / 60) * 360) + ((seconds / 60) * 6) + 90);; /* plus 90deg is to account for initial offset */
+    const hoursToDegrees = (((hours / 12) * 360) + ((mins / 60) * 30) + 90); /* plus 90deg is to account for initial offset */
     secondHand.css('transform', `rotate(${secondsToDegrees}deg)`);
     minsHand.css('transform', `rotate(${minsToDegrees}deg)`);
     hoursHand.css('transform', `rotate(${hoursToDegrees}deg)`);
