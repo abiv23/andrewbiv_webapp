4fb50c0f5483ae86dad52c3be2a566ef38659fcb
“Andrew <abiv23@gmail.com>
2018-02-03 10:44:48 -0700

    ::tada:: javascript added, player draft list management completed

--- scripts/fantasy-draft.js
+++ scripts/fantasy-draft.js
@@ -1,20 +1,22 @@
 $(document).ready(function() {
   $(".draft-player").hide();
   $(".player").hover(playerSelected, playerDeselected);
-
 })
 
 function playerSelected(event){
   console.log(event.target.innerHTML);
-  // $(".player").removeClass('draft-player');
-  // $(".player").removeClass('select-player');
-  // $(this).removeClass('active');
   $(this).addClass('select-player');
   $(this).find(".draft-player").show();
-  // $(this).off('click', playerSelected);
+  $(this).click(draftPlayer);
 }
 
 function playerDeselected(event){
-  $(this).removeClass('select-player');
-    $(this).find(".draft-player").hide();
+  $(this).removeClass("select-player");
+  $(this).find(".draft-player").hide();
+}
+
+function draftPlayer(event){
+  event.preventDefault();
+  const playerCard = event.currentTarget;
+  playerCard.remove();
 }
